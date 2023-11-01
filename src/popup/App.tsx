18ffe11ccafb8e833c9317b9ba20bd18bad1dea3
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-11-01 14:42:14 +0100

    Removed dark mode setting for themes not supporting it

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -12,13 +12,13 @@ function App() {
   });
   useEffect(() => {
     chrome.storage.local.get(["theme", "darkMode"], (result) => {
-      setSettings({...settings,...result});
+      setSettings({ ...settings, ...result });
     });
   }, []);
 
-  function update(key:string, value:string|boolean){
-    chrome.storage.local.set({[key]: value}, () => {
-      setSettings({...settings,[key]: value});
+  function update(key: string, value: string | boolean) {
+    chrome.storage.local.set({ [key]: value }, () => {
+      setSettings({ ...settings, [key]: value });
     });
   }
 
@@ -28,22 +28,36 @@ function App() {
         <img src={"/images/logo-128.png"} width={50} height={50} />
         <h2>Options</h2>
       </div>
-
       <div className="nice-form-group">
         <label htmlFor="themes">Theme:</label>
-        <select name="themes" id="themes" value={settings.theme} onChange={(e) => update("theme", e.currentTarget.value)}>
+        <select
+          name="themes"
+          id="themes"
+          value={settings.theme}
+          onChange={(e) => update("theme", e.currentTarget.value)}
+        >
           {Object.keys(themes).map((theme) => (
-            <option key={theme} value={theme}>{theme}</option>
+            <option key={theme} value={theme}>
+              {theme}
+            </option>
           ))}
         </select>
       </div>
-      <div className="nice-form-group">
-        <input type="checkbox" id="darkMode" className="switch" checked={settings.darkMode} onChange={(e) => update("darkMode", e.currentTarget.checked)}/>
-        <label htmlFor="darkMode">
-          Enable dark mode
-          <small>Only works if the selected theme supports it</small>
-        </label>
-      </div>
+      {themes[settings.theme].hasDarkMode && (
+        <div className="nice-form-group">
+          <input
+            type="checkbox"
+            id="darkMode"
+            className="switch"
+            checked={settings.darkMode}
+            onChange={(e) => update("darkMode", e.currentTarget.checked)}
+          />
+          <label htmlFor="darkMode">
+            Enable dark mode
+            <small>Only works if the selected theme supports it</small>
+          </label>
+        </div>
+      )}
     </>
   );
 }
