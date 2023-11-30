fbe6690f13c3af64abfd4eeb76c8bbd07e7ed819
Philip Kristian Møller Flyvholm <phif@itu.dk>
2023-11-30 12:44:16 +0100

    Added feat: #58

--- src/styles/hacker/styles.ts
+++ src/styles/hacker/styles.ts
@@ -1,4 +1,13 @@
 import baseDarkCss from "data-text:../lpp/dark.css";
 import hackerSpecific from "data-text:./main.css";
+import cursor from "data-base64:~assets/images/cursors/hacker/cursor_normal.png"
+import cursor_pointer from "data-base64:~assets/images/cursors/hacker/cursor_pointer.png"
 
-export default (baseDarkCss + hackerSpecific);
+const cursorStyle = `
+:root {
+  --custom-cursor: url(${cursor});
+  --custom-cursor-pointer: url(${cursor_pointer});
+}
+`
+
+export default (baseDarkCss + cursorStyle + hackerSpecific);
