04a0f75fa8820b0f3170949260d40f657a79b905
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2024-05-26 11:11:20 +0200

    update alert-info banner (#76)

--- src/styles/hacker/styles.ts
+++ src/styles/hacker/styles.ts
@@ -1,7 +1,7 @@
 import baseDarkCss from "data-text:../lpp/dark.css";
 import hackerSpecific from "data-text:./main.css";
-import cursor from "data-base64:~assets/images/cursors/hacker/cursor_normal.png"
-import cursor_pointer from "data-base64:~assets/images/cursors/hacker/cursor_pointer.png"
+import cursor from "data-base64:assets/images/cursors/hacker/cursor_normal.png"
+import cursor_pointer from "data-base64:assets/images/cursors/hacker/cursor_pointer.png"
 
 const cursorStyle = `
 :root {
