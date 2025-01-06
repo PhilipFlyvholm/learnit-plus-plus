efcc98b4ddbc0bd2b49ed20c107c179007b51083
Philip Flyvholm <philipflyvholm@gmail.com>
2025-01-06 15:38:44 +0100

    Removed unused console logs

--- src/studentcouncil/eventLoader.ts
+++ src/studentcouncil/eventLoader.ts
@@ -1,24 +1,24 @@
-import ICAL from "ical.js";
+import ICAL from "ical.js"
+
 //webcal://studentcouncil.dk/subscribe
 export type EventData = {
-  summary: string;
-  dtstart: string;
-  dtend: string;
-  location: string;
-  description: string;
-  url: string;
-};
-console.log("Studentcouncil enabled");
+  summary: string
+  dtstart: string
+  dtend: string
+  location: string
+  description: string
+  url: string
+}
+console.log("Studentcouncil enabled")
 export async function getEvents() {
-  const response = await fetch("https://studentcouncil.dk/subscribe");
+  const response = await fetch("https://studentcouncil.dk/subscribe")
   if (!response.ok) {
-    return [];
+    return []
   }
-  const text = await response.text();
-  const data = ICAL.parse(text);
-  console.log(data);
-  const cal = new ICAL.Component(data);
-  const events: EventData[] = [];
+  const text = await response.text()
+  const data = ICAL.parse(text)
+  const cal = new ICAL.Component(data)
+  const events: EventData[] = []
   cal.getAllSubcomponents("vevent").forEach((event) => {
     const eventData: EventData = {
       summary: event.getFirstPropertyValue("summary"),
@@ -26,15 +26,15 @@ export async function getEvents() {
       dtstart: event.getFirstPropertyValue("dtstart"),
       dtend: event.getFirstPropertyValue("dtend"),
       location: event.getFirstPropertyValue("location"),
-      description: event.getFirstPropertyValue("description"),
-    };
+      description: event.getFirstPropertyValue("description")
+    }
     //Only push if the event is in the future
-    if (new Date(eventData.dtend) < new Date()) return;
-    events.push(eventData);
-  });
+    if (new Date(eventData.dtend) < new Date()) return
+    events.push(eventData)
+  })
   //Sort by date
   events.sort((a, b) => {
-    return new Date(a.dtstart).getTime() - new Date(b.dtstart).getTime();
-  });
-  return events;
+    return new Date(a.dtstart).getTime() - new Date(b.dtstart).getTime()
+  })
+  return events
 }
