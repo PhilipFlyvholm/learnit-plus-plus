a1147b69a601983f1149086410338d1d24a1678f
Philip Flyvholm <36300927+PhilipFlyvholm@users.noreply.github.com>
2025-01-08 11:53:14 +0100

    Better warning handling (#103)

--- src/components/calendar/calendarHeader.tsx
+++ src/components/calendar/calendarHeader.tsx
@@ -1,8 +1,28 @@
-const CalendarHeader = ({toggleView}:{toggleView:() => void}) => {
+import { useCalendarSettings } from "./calendarHooks"
+
+const CalendarHeader = ({ toggleView }: { toggleView: () => void }) => {
+  const [settings, _setSettings, { isLoading: isLoadingSettings }] =
+    useCalendarSettings()
   return (
-    <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: ".5rem" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: ".5rem"
+      }}>
       <h5 className="card-title d-block m-0">Calendar</h5>
-      <div style={{ display: "flex", justifyContent: "end" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "end",
+          alignItems: "center",
+          gap: ".5rem"
+        }}>
+        {settings.icalSources.length == 0 && (
+          <p className="bold">No custom calendars setup yet!</p>
+        )}
+
         <button className="btn btn-outline-secondary" onClick={toggleView}>
           Settings
         </button>
